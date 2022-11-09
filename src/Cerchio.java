e8a37227296f434d419f7faa8dd50f7c57ea7e44
Riccardo <richitrebbia@gmail.com>
2022-11-09 12:38:47 +0100

    added hashCode and Equal

--- src/Cerchio.java
+++ src/Cerchio.java
@@ -24,6 +24,30 @@ public class Cerchio extends Forma {
     public double perimetro() {
         return extracted();
     }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(raggio);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cerchio other = (Cerchio) obj;
+        if (Double.doubleToLongBits(raggio) != Double.doubleToLongBits(other.raggio))
+            return false;
+        return true;
+    }
 
     public void setRaggio(double raggio) {
         this.raggio = raggio;
